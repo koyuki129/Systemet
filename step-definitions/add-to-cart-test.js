bf26f659cfe3752ec2b99135293ba235277cf1aa
Sabina <sabina-ericsson@hotmail.com>
2019-04-24 13:32:46 +0200

    ok test

---
+++ step-definitions/add-to-cart-test.js
@@ -0,0 +1,10 @@
+let shoppingCart = require('www\js\shopping-cart.js');
+let product = require('www\js\product.js'); 
+
+module.exports = new Product
+
+
+this.Given(/^that the product is available in the store$/, function () {
+
+  });
+
