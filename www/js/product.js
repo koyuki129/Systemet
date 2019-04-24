6f5d0cc23fabe75e75223e0b962c8e20d16bc5e2
Sabina <sabina-ericsson@hotmail.com>
2019-04-24 11:18:00 +0200

    Product

--- www/js/product.js
+++ www/js/product.js
@@ -1,9 +1,13 @@
-module.exports = class Product {
-
-  constructor(){
-
-
-    
+class Product {
+ 
+  constructor(data){
+    Object.assign(this, data);
   }
+ 
+}
 
+let temp = require('./sortiment.json');
+let products = [];
+for(let t of temp){
+  products.push(new Product(t));
 }
