47adc17dc7853c073a5f9c799eac84947fa96b52
Sabina <sabina-ericsson@hotmail.com>
2019-05-15 11:19:05 +0200

    add-to-cart-test

---
+++ step-definitions/selenium-add-to-cart.js
@@ -0,0 +1,64 @@
+let { $, sleep } = require('./funcs.js');
+module.exports = function () {
+
+        this.Given(/^that the products are available in the store$/, function () {
+
+        });
+
+        this.When(/^I add on product to the cart$/, async function () {
+
+                let searchBar = await $('.search #search');
+
+                await searchBar.sendKeys("Öl")
+                let add = await $('.search-page .add');
+
+                assert.notEqual(add, null, 'Could not find the addbuttom');
+                await add.click()
+
+        });
+
+
+        this.Then(/^i should see the product in the cart$/, async function () {
+
+                let cartItems = await $('.cart-page *');
+                assert(cartItems, 'Öl', "The cart does not contain any products")
+        });
+
+
+        this.When(/^I add one other product to the cart$/, async function () {
+
+                let searchBar = await $('.search #search');
+                await searchBar.sendKeys("Vin")
+                let add = await $('.search-page .add');
+
+                assert.notEqual(add, null, 'Could not find the addbuttom');
+                await add.click()
+        });
+
+        this.Then(/^I should see the products in the cart$/, async function () {
+
+                let cartItems = await $('.cart-page *');
+                assert(cartItems, 'Öl', "The cart does not contain any products")
+                assert(cartItems, 'Vin', "The cart does not contain any products")
+        });
+
+
+        this.Given(/^I add one product that is out of stock to the cart$/, async function () {
+                let outOfStock = await $('.search-page .outOfStock');
+                let searchBar = await $('.search #search');
+                await searchBar.sendKeys("Smirnoff");
+
+                assert.notEqual(outOfStock, null, 'Could not find the addbuttom');
+                await outOfStock.click();
+        });
+
+        this.Then(/^the page should show a message$/, async function () {
+                let outOfStock = await $('.search-page .outOfStock');
+
+                assert.notEqual(outOfStock, 'Tillfälligt slut', 'Your order should show that it is not in stock');
+                await outOfStock.click();
+        });
+
+
+
+}
--- www/js/GUI/gui-shopping-cart.js
+++ www/js/GUI/gui-shopping-cart.js
@@ -1,37 +1,47 @@
-class GuiShoppingCart{
+class GuiShoppingCart {
 
     constructor() {
         //$('.start-page').hide();
-       // $('.search-page').hide();
-       // $('.cart-page').show();
+        // $('.search-page').hide();
+        // $('.cart-page').show();
         this.updateListOfProducts();
         $('.emptyCart').click((e) => {
-            
+
         });
-        
+
         $('.add').click((e) => {
             cart.add();
         });
-       
+
     }
 
+    add() {
+        cart.add();
 
-    remove(){
+
+    }
+
+    outOfstock(){
+        cart.add();
+    }
+    
+
+    remove() {
         cart.remove();
     }
 
 
-    updateListOfProducts(){
+    updateListOfProducts() {
         // loop thru items in thingsToBuy
         // then add their html-code to .cart-items
 
-      //  let itemsHtml = "<h3>Rödvin</h3><button>Buy</button>";
-
-        
-       // $('.cart-items').html(itemsHtml);
+        //  let itemsHtml = "<h3>Rödvin</h3><button>Buy</button>";
 
 
-    } 
+        // $('.cart-items').html(itemsHtml);
+
+
+    }
 }
 
 
