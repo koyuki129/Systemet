1f7bb26f5c18d22fe8edd1bd08177a7b52d12d28
Sabina <sabina-ericsson@hotmail.com>
2019-05-16 15:07:15 +0200

    addS

--- step-definitions/selenium-add-to-cart.js
+++ step-definitions/selenium-add-to-cart.js
@@ -1,5 +1,8 @@
 let { $, sleep } = require('./funcs.js');
 module.exports = function () {
+        let addedProduct1;
+        let addedProduct2;
+        let outOfStockProduct;
 
         this.Given(/^that the products are available in the store$/, function () {
 
@@ -21,22 +24,26 @@ module.exports = function () {
 
                 await add.click();
 
+                let firstProduct = await $('.cart-items td:first-child');
+                addedProduct1 = await firstProduct.getText();
+
         });
 
 
         this.Then(/^i should see the product in the cart$/, async function () {
-                let cartItems = await $( '.cart-items');
-                let product = await $('td:first-child');
 
-                for(let product of cartItems){
-                        return product
-                }
+                let cartItems = await $('.cart-items td:first-child');                
+                assert((await cartItems.getText()).includes(addedProduct1), "The text is not Auld Rare Benriach");
+                //let product = await $('td:first-child');
 
-                assert(product.get_attribute("Auld Rare Benriach"), "The text is not Auld Rare Benriach");
 
-                //let element = await $('... etc');
+
+                //assert(product.getText() === 'Auld Rare Benriach', "The text is not Auld Rare Benriach");
+
+
                 //assert(element.getText() === 'Brännvinn', "The text is not Brännvin");
                 //Ifall .getText() 'inte skulle funka så använd instället metoden .get_attribute("innerText");
+
                 //$('table.cart-items tbody td:first-child')[0].innerText
                 //assert(cartItems, 'Auld Rare Benriach', "The cart does not contain any products")
 
@@ -44,29 +51,18 @@ module.exports = function () {
         });
 
 
-        this.When(/^I add one other product to the cart$/, async function () {
-                let searchBar = await $('.search #search');
-                let searchButton = await $('.search .searchbutton');
-
-                await searchBar.sendKeys("Moscato d'Asti");
-                await searchButton.click();
-
-                let add = await $('.search-page .add');
-                if(add.length > 0){
-                add = add[0];
-                }
-
-                assert.notEqual(add, null, 'Could not find the addbuttom');
-
-                await add.click();
-
-        });
+  
 
         this.Then(/^I should see the products in the cart$/, async function () {
+                let cartItems = await $('.cart-items td:first-child');                
+                assert((await cartItems.getText()).includes(addedProduct1), "The text is not Auld Rare Benriach");
+                console.log(cartItems)
+                 cartItems = await $('.cart-items td:first-child');                
+                assert((await cartItems.getText()).includes(addedProduct2), "The text is not Auld Rare Benriach");
 
-                let cartItems = await $('.cart-page *');
-                assert(cartItems, 'Öl', "The cart does not contain any products")
-                assert(cartItems, 'Vin', "The cart does not contain any products")
+      
+
+
         });
 
 
