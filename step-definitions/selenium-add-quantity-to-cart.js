49e4c1a009d2b9be8c9048f971b8982899817b02
Kisanet <kisanet80@yahoo.se>
2019-05-20 21:56:41 +0200

    Add Quantity

--- step-definitions/selenium-add-quantity-to-cart.js
+++ step-definitions/selenium-add-quantity-to-cart.js
@@ -10,36 +10,37 @@ module.exports = function () {
 //When I add 2 units of the same products to the cart
 
  this.When(/^I add (\d+) units of the same products to the cart$/, async function (units) {
+  
+ 
   let searchBox = await $('.search #search');
   await searchBox.sendKeys('Cava');
   
   let searchButton = await $('.searchbutton')
   await searchButton.click();
 
-  let addButton = await $('.add')
-  await addButton.click();
-
-  let a = await $('.a')
-  await a.click;
  
 
-  let button = await $('.button')
-  await button.click();
+  let add = await $('.add')
+  await add[0].click();
+  await add[0].click();
+ 
  });
 
 //Then the products should be added to the cart
 
 this.Then(/^the products should be added to the cart$/, async function () {
-  let search = await $('.search-page');
-  assert.equal(search, [2], 'The product should not be added!');
+  let searchBox = await $('.search #search');
+  await searchBox.sendKeys('Cava');
+  //( 'The unit should not be added!');
 });
 
 //And the quantity of the products in the cart is 2 
 
-this.Then(/^the quantity of the products in the cart is (\d+)$/, async function (quantity) {
-  let esarch = await $('.search-page');
-  let quantity = await cart.quantity('');
-  assert.equal(quantity, '2', 'The quantity of the product is null!');
+this.Then(/^the quantity of the products in the cart is (\d+)$/, async function (units) {
+  let searchButton = await $('.searchbutton');
+  await searchButton.sendKeys('Cava');
+  await searchButton.sendKeys('Cava');
+  //( 'The quantity should not be added!');
 }); 
 
 // Senario nr 2
@@ -47,25 +48,46 @@ this.Then(/^the quantity of the products in the cart is (\d+)$/, async function 
 //Given that I am on the web page localhost:3000
 
 //And that the the products are available in the store
-this.Given(/^that the the products are available in the store$/, function (callback) {
+this.Then(/^that the the products are available in the store$/, function () {
+
+        
+
+
 });
 
 //When I add 1 unit of the same products to the cart
 
-this.When(/^I add (\d+) unit of the same products to the cart$/, async function () {
-  let addUnit = await $('#add');
-  let theButton = await $('.searchbutton');
+this.When(/^I add (\d+) unit of the same products to the cart$/, async function (unit) {
 
-   await addUnit.sendKeys("Cava");
-   await theButton.click();
+  let searchBox = await $('.search #search');
+  await searchBox.sendKeys('Cava');
  
+  let searchButton = await $('.searchbutton')
+  await searchButton.click();
+
+
+  let add = await $('.add')
+
+  await add[1].click();
 });
  
 //And I add 1 unit of the same products to the cart
-//And I add 1 unit of the same products to the cart
-//Then the products should be added to the cart
 
+
+  //And I add 1 unit of the same products to the cart
+  
+//Then the products should be added to the cart
+ 
 //And the quantity of the products in the cart is 3
+this.Then(/^the quantity of the products in the cart is(\d+) $/, async function() {
+  let shoppingCart = await $('.shopping-cart');
+  let theButtom = await $('.div')
+  await shoppingCart.click();
+  await theButtom.click();
+    
+  
+  
+});
 
  // Scenario nr 3:  
   
@@ -75,11 +97,17 @@ this.When(/^I add (\d+) unit of the same products to the cart$/, async function 
 
  //When I add 0 unit of the products to the cart
 
- this.When(/^I add (\d+) unit of the products to the cart$/, function (arg1, callback) {
- });
+ this.When(/^I add (\d+) unit of the products to the cart$/, async function (unit) {
+  let searchBox = await $('.search #search');
+  await searchBox.sendKeys('0');
+  
+  let searchButton = await $('.searchbutton')
+  await searchButton.click();
+
+});
 
  //Then the products should not be added to the cart
 
- this.Then(/^the products should not be added to the cart$/, function (callback) {
+ this.Then(/^the products should not be added to the cart$/, function () {
  }); 
 }
