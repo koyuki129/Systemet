77c046ca94eadfc039b15feb7258a53e19764c69
Bairez <mailboxkarina@gmail.com>
2019-05-20 18:01:39 +0200

    ändrat på open-with same-cart

--- step-definitions/open-with-same-cart.js
+++ step-definitions/open-with-same-cart.js
@@ -1,5 +1,5 @@
-/*let Product = require('../www/js/product.js');
-let ShoppingCart = require('../www/js/shopping-cart.js');
+
+let { $ } = require('./funcs.js');
 
 module.exports = function () {
 
@@ -7,15 +7,26 @@ module.exports = function () {
   let cart;
   
 
-  this.Given(/^that there is at least one products in the cart$/, function () {
-    cart = new ShoppingCart();
-    cart.add(Product.products[18], 1);
+  this.Given(/^that there is at least one products in the cart$/, async function () {
+    
+    let searchBar = await $('.search #search');
+    let theButtom = await $('.search .searchbutton');
+    await searchBar.sendKeys("Öl")
+    await theButtom.click();
+    let add = await $('.search-page .add');
+    if (add.length > 0) {
+      add = add[0];
+    }
+
+    assert.notEqual(add, null, 'Could not find the addbuttom');
+    await add.click()
   });
 
   this.When(/^I close the browser$/, function () {
-
+  });
 
   this.When(/^reopen the browser$/, function () {
+    
+  });
 
-
-}*/
+}
