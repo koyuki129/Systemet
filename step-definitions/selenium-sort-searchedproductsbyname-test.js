6d6fe18ade94b7b993a6d232a7e0449c33af66b4
Shilpa <shilpadahiya87@gmail.com>
2019-05-21 15:02:17 +0200

    sort product by name  test  start

--- step-definitions/selenium-sort-searchedproductsbyname-test.js
+++ step-definitions/selenium-sort-searchedproductsbyname-test.js
@@ -3,14 +3,13 @@ let { $, sleep } = require('./funcs.js');
 module.exports = function () {
     this.Given(/^that I have a list of search results$/, async function () {
         let searchBar = await $('.search #search');
-        await searchBar.sendKeys();
+        await searchBar.sendKeys("Renat");
         let searchButton = await $('.search .searchbutton');
         await searchButton.click();
+        let searchPage = await $('.search-page');
+        await searchPage.getText();
+
       });
 
-      this.When(/^I press alphabetical order button$/, async function () {
-        let alphabeticalOrderButton = await $('.search-page .sortera .Namn A till Ö');
-        assert.notEqual(alphabeticalOrderButton, null, "Could not find the alphabeticalorder button");
-        await alphabeticalOrderButton.click();
-      });
+     
 }
