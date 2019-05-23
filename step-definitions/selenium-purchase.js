499f5b60482e42b1e42f26537d7e22a4540a551b
Sabina <sabina-ericsson@hotmail.com>
2019-05-23 11:07:43 +0200

    purchase

--- step-definitions/selenium-purchase.js
+++ step-definitions/selenium-purchase.js
@@ -3,7 +3,10 @@ module.exports = function () {
 
 
     this.When(/^I press order button$/, async function () {
-        this.sum = 0; // calculate the total price to pay
+        this.totPrice = await $('.totPrice');
+
+        this.totPrice = await this.totPrice.getText();
+        this.totPrice = Number(this.totPrice);
 
         let checkoutButton = await $('.cart-page .checkout');
         assert.notEqual(checkoutButton, null, 'Could not find the checkout button');
@@ -14,11 +17,18 @@ module.exports = function () {
       this.Then(/^I should get a reciept that the products are bought including the total price paid$/, async function () {
           let reciept = await $('.reciept')
           reciept = await reciept.getText();
-          assert(reciept.includes(this.sum), 'sum does not work');
-
-
+          assert(reciept.includes(this.totPrice), 'sum does not work');
 
       });
+
+      this.Then(/^I should get a reciept that (\d+) products are bought including the total price paid$/, async function () {
+        let reciept = await $('.reciept')
+        reciept = await reciept.getText();
+        assert(reciept.includes(this.totPrice), 'sum does not work');
+        assert(reciept.includes(this.totPrice), 'sum does not work');
+
+      });
+
 
 
 
