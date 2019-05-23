b782a8068d3464044963abf1afe668179759910c
Sabina <sabina-ericsson@hotmail.com>
2019-05-23 09:51:19 +0200

    snart klar-purchase

--- step-definitions/selenium-purchase.js
+++ step-definitions/selenium-purchase.js
@@ -5,9 +5,6 @@ module.exports = function () {
     this.When(/^I press order button$/, async function () {
         this.sum = 0; // calculate the total price to pay
 
-    
-        
-
         let checkoutButton = await $('.cart-page .checkout');
         assert.notEqual(checkoutButton, null, 'Could not find the checkout button');
         await checkoutButton.click();
@@ -17,11 +14,15 @@ module.exports = function () {
       this.Then(/^I should get a reciept that the products are bought including the total price paid$/, async function () {
           let reciept = await $('.reciept')
           reciept = await reciept.getText();
-          assert(reciept.includes(this.sum), 'sumOfProducts does not work');
+          assert(reciept.includes(this.sum), 'sum does not work');
 
 
 
       });
+
+
+
+      
 
 
 
