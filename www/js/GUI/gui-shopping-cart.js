e55b034d1a977b510c8c925cd88d1d74d37bc762
Arzuhej <arzu.ogu@gmail.com>
2019-05-28 16:16:37 +0200

    design klar

--- www/js/GUI/gui-shopping-cart.js
+++ www/js/GUI/gui-shopping-cart.js
@@ -1,13 +1,14 @@
 class GuiShoppingCart {
 
     constructor() {
-        //$('.start-page').hide();
+        //$('.start-p((age').hide();
         // $('.search-page').hide();
         // $('.cart-page').show();
         window.cart = this.cart = new ShoppingCart();
         this.updateListOfProducts();
         $('.error').hide();
-        $('.totPrice').text(this.cart.sumOfProducts());
+        $('.totPrice').text(this.cart.sumOfProducts())
+
 
         // När man skapar en eventhanterare så här:
         // $('.add').click((e) => {});
@@ -16,6 +17,11 @@ class GuiShoppingCart {
         // Gör man istället så här
         // $(document).on('click', '.add', (e) => {});
         // gäller det alla KOMMANDE/ej än existerande element också
+
+        $(document).on('click', '.shoppingcart', () => {
+            let topPosOfCartPage = $('.cart-page').offset().top;
+            window.scrollTo(0, topPosOfCartPage);
+        });
 
         $(document).on('click', '.add', (e) => {
             let theButtonClicked = $(e.currentTarget);
@@ -27,12 +33,9 @@ class GuiShoppingCart {
             } else {
                 $('.error').hide();
             }
-
-            setTimeout(function () { $('.error').fadeOut(); }, 3000);
-
-            $('.totPrice').text(this.cart.sumOfProducts());
+            $('.totPrice').text(this.cart.sumOfProducts())
+            
         });
-
         $(document).on('click', '.remove', (e) => {
             let theButtonClicked = $(e.currentTarget);
             let row = theButtonClicked.closest('tr');
@@ -41,7 +44,6 @@ class GuiShoppingCart {
             this.updateListOfProducts();
             $('.totPrice').text(this.cart.sumOfProducts())
         });
-
         $(document).on('click', '.raise', (e) => {
             let theButtonClicked = $(e.currentTarget);
             let row = theButtonClicked.closest('tr');
@@ -50,7 +52,6 @@ class GuiShoppingCart {
             this.updateListOfProducts();
             $('.totPrice').text(this.cart.sumOfProducts())
         });
-
         $(document).on('click', '.lower', (e) => {
             let theButtonClicked = $(e.currentTarget);
             let row = theButtonClicked.closest('tr');
@@ -59,7 +60,6 @@ class GuiShoppingCart {
             this.updateListOfProducts();
             $('.totPrice').text(this.cart.sumOfProducts())
         });
-
         $(document).on('change', 'td input', (e) => {
             let theSubmitted = $(e.currentTarget);
             let row = theSubmitted.closest('tr');
@@ -69,40 +69,29 @@ class GuiShoppingCart {
             $('.totPrice').text(this.cart.sumOfProducts())
         });
 
-
-        $(document).on('click', '.emptycart button', (e) => {
+       $(document).on('click', '.emptycart button', (e) => {
             this.cart.emptyCart();
-            $('.message').html(`<div class="alert alert-success" role="alert">
-            <h4 class="alert-heading">Varukorgen är töm</h4>
-            </div></p>`)
-
-            setTimeout(function () { $('.alert').fadeOut(); }, 3000);
-            $('.totPrice').text(this.cart.sumOfProducts());
-
-            this.updateListOfProducts();
-            
+            this.updateListOfProducts();          
         });
+
+    /*    $(document).on('change', 'totProduct', (e) => {
+            let theSubmitted = $(e.currentTarget);
+            let row = theSubmitted.closest('tr');
+            let product = row.data('product');
+            this.cart.editQuantity(product, theSubmitted.val() / 1);
+            this.updateListOfProducts();
+            $('.totPrice').text(this.cart.sumOfProducts())
+        });*/
 
 
         $(document).on('click', '.checkout button', (e) => {
-            $('.receipt').html(`<div class="alert alert-success" role="alert">
+            $('.reciept').html(`<div class="alert alert-success" role="alert">
             <h4 class="alert-heading">Tack för din beställning!</h4>
             <p>Aww yeah, Dina varor kommer att anlända inom 3 arbetsdagar 
             Summa för beställda produkter:</p>
-            </div> ${this.cart.sumOfProducts()}</p>`)
-            if (this.cart.thingsToBuy <= 0) {
-                $('.receipt').hide();
-            } else {
-                $('.receipt').show();
-            }
-
-            setTimeout(function () { $('.alert').fadeOut(); }, 5000);
-
+          </div> ${this.cart.sumOfProducts()}</p>`)
             this.cart.checkout();
             this.updateListOfProducts();
-
-            setTimeout(function () { $('.totPrice').fadeOut(); }, 5000);
-            
         });
     }
 
