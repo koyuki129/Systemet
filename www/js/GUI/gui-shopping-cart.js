b0d0f588e30ae669d77e64cf214a5dc7e036e0d2
Koyuki129 <salehi88@hotmail.com>
2019-05-28 14:48:40 +0200

    liten fix till checkout

--- www/js/GUI/gui-shopping-cart.js
+++ www/js/GUI/gui-shopping-cart.js
@@ -88,7 +88,7 @@ class GuiShoppingCart {
             $('.receipt').html(`<div class="alert alert-success" role="alert">
             <h4 class="alert-heading">Tack för din beställning!</h4>
             <p>Dina varor kommer att anlända inom 3 arbetsdagar. Tänk på att varor som inte finns i lager kan ta upp till en vecka att anlända. </p>
-            </div> ${this.cart.sumOfProducts()}</p>`)
+             Ditt total pris är: ${this.cart.sumOfProducts()}</div></p>`)
             if (this.cart.thingsToBuy <= 0) {
                 $('.receipt').hide();
             } else {
