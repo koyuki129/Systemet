261c10bd61754a30954c55faea2c031a1dcc92db
danielyo <salehi88@hotmail.com>
2019-06-05 12:32:01 +0200

    change language to English and removal of empty files

--- step-definitions/selenium-removetest.js
+++ step-definitions/selenium-removetest.js
@@ -157,9 +157,9 @@ module.exports = function () {
 
 this.Then(/^the cart should not contain the removed product anymore$/, async function () {
   let cartItems = await $('.cart-items td:first-child');
-    assert.notEqual(cartItems, null, 'Det finns tydligen något kvar');
-    assert(Array.isArray(cartItems) == false, 'Det var tydligen en array');
-    assert(!(await cartItems.getText()).includes(this.addedProduct1), "Det finns tydligen något kvar")
+    assert.notEqual(cartItems, null, 'There is apparantly something left');
+    assert(Array.isArray(cartItems) == false, 'That was apparantly an array');
+    assert(!(await cartItems.getText()).includes(this.addedProduct1), 'There is apparantly something left')
 
 });
 }
